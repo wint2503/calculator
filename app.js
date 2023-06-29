6757d186d9d86bc60fcc7cc73b99e2a3bce6ccd1
Wint Thanda Moe <wintthandamoe@gmail.com>
2023-06-29 23:12:08 +0630

    template

---
+++ app.js
@@ -0,0 +1,54 @@
+const getone = document.querySelector('#one');
+const gettwo = document.getElementById('two');
+const getthree = document.getElementById('three');
+const getfour = document.getElementById('four');
+const getfive = document.getElementById('five');
+const getsix = document.getElementById('six');
+const getseven = document.getElementById('seven');
+const geteight = document.getElementById('eight');
+const getnine = document.getElementById('nine');
+
+const getplus = document.getElementById('plus');
+const getequal = document.getElementById('equal');
+const getdisplay = document.querySelector('#number');
+
+// const getonevalue = ()=>{
+//     console.log('hello')
+// }
+// getone.addEventListener('click',getonevalue);
+
+
+getone.addEventListener('click',function(){
+    const getdatas = getone.value;
+    getdisplay.value += getdatas;
+})
+
+gettwo.addEventListener('click',function(){
+    const getdatas = gettwo.value;
+    getdisplay.value += getdatas;
+})
+
+gettwo.addEventListener('click',function(){
+    const getdatas = gettwo.value;
+    getdisplay.value += getdatas;
+})
+
+gettwo.addEventListener('click',function(){
+    const getdatas = gettwo.value;
+    getdisplay.value += getdatas;
+})
+
+gettwo.addEventListener('click',function(){
+    const getdatas = gettwo.value;
+    getdisplay.value += getdatas;
+})
+
+gettwo.addEventListener('click',function(){
+    const getdatas = gettwo.value;
+    getdisplay.value += getdatas;
+})
+
+gettwo.addEventListener('click',function(){
+    const getdatas = gettwo.value;
+    getdisplay.value += getdatas;
+})
